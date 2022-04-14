c8b212c1ac50c56713d7319518dd530d694d464d
alexazarh <aazarh@gmail.com>
2022-04-14 16:30:13 -0500

    update

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -2,5 +2,6 @@ output "hostname" {
   value = "${aws_db_instance.default.endpoint}"
 }
 output "connection_string" {
+  sensitive = true
   value = "Server=${aws_db_instance.default.address};Port=${aws_db_instance.default.port};Database=${var.db_name};Uid=${var.username};Pwd=${var.password};"
 }
